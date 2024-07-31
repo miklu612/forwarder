67873cd169d438baa2ea696b2728c543054cd7f4
Michael <robo.dude@mail.com>
2024-07-31 16:21:41 +0300

    Simple implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::Html, 
+    body::Body,
+    extract::Request,
+    routing::{get, MethodRouter}, 
+    Router
+};
 use std::env;
 use std::fs;
-use serde_json::{Value};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
@@ -22,15 +28,20 @@ async fn main() {
     app = app.fallback(get(error));
 
     for path in config.as_array().unwrap() {
-        app = app.route(path["path"].as_str().unwrap(), get(handler));
+        let new_path = "http://127.0.0.1:".to_string() + path["port"].as_str().unwrap();
+        println!("Getting path: {}", new_path);
+        let m_router: MethodRouter = get(|request: Request<Body>| async move {
+            let headers = request.headers();
+            println!("{:?}", headers);
+            let body = reqwest::get(new_path).await.unwrap().text().await.unwrap();
+            return Html(body);
+        });
+        app = app.route(path["path"].as_str().unwrap(), get(m_router));
     }
-
-
-
-
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
 }
 
 async fn error() -> Html<&'static str> {
