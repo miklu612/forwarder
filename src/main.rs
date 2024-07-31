b75adcfa52efaddffc298618b732523c09a598b3
Michael <robo.dude@mail.com>
2024-07-31 18:38:56 +0300

    Fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -88,27 +88,32 @@ async fn main() {
 
     let config_data: Value = serde_json::from_str(&json_raw).expect("Failed to parse json");
     let config = config_data["config"].clone();
+    let binding = config_data["url"].clone();
+    let local_ip = Arc::new(Mutex::new(binding.as_str().unwrap()));
+    let root_url = Arc::new(Mutex::new("http://".to_string() + &local_ip.lock().unwrap().clone()));
 
     let mut app = Router::new();
     // TODO: Add mutex
     let connections = Arc::new(Mutex::new([();20].map(|_| IPPortPair::new())));
    
-    let a = connections.clone();
-    let b = connections.clone();
-    app = app.fallback(get(|connection_info: ConnectInfo<SocketAddr>, request: Request<Body> | async move {
-        let vec = &mut a.lock().unwrap().clone();
-        let ip = format!("{}", connection_info.0);
-        let index = vec.clone().into_iter().position(|x| {x.get_ip() == ip});
-        if let Some(v) = index {
-            let path = vec[v].clone().get_port();
-            let original_path = request.uri().path();
-            let new_path = "http://127.0.0.1:".to_string() + &path + original_path;
-            println!("{}", new_path);
-            let body = reqwest::get(new_path).await.unwrap().text().await.unwrap();
-            return Html(body);
-        }
-        Html("Forwarder: Error".to_string())
-    }));
+    {
+        let local_connections = connections.clone();
+        let local_root_url = root_url.clone();
+        app = app.fallback(get(|connection_info: ConnectInfo<SocketAddr>, request: Request<Body> | async move {
+            let vec = &mut local_connections.lock().unwrap().clone();
+            let ip = format!("{}", connection_info.0);
+            let index = vec.clone().into_iter().position(|x| {x.get_ip() == ip});
+            if let Some(v) = index {
+                let path = vec[v].clone().get_port();
+                let original_path = request.uri().path();
+                let new_path = local_root_url.lock().unwrap().clone() + ":" + &path + original_path;
+                println!("{}", new_path);
+                let body = reqwest::get(new_path).await.unwrap().text().await.unwrap();
+                return Html(body);
+            }
+            Html("Forwarder: Error".to_string())
+        }));
+    }
 
     for path in config.as_array().unwrap() {
         // This part will just basically log the ip and store it. This ip is then used to track
@@ -117,7 +122,7 @@ async fn main() {
         let og_port = path["port"].as_str().unwrap();
         let mut outer_information = Box::new(IPPortPair::new());
         outer_information.set_port(og_port.to_string());
-        let new_path = "http://127.0.0.1:".to_string() + path["port"].as_str().unwrap();
+        let new_path = root_url.lock().unwrap().clone() + ":" + path["port"].as_str().unwrap();
         let c = connections.clone();
         let m_router: MethodRouter = get(|connect_info: ConnectInfo<SocketAddr>| async move {
             let ip = connect_info.0;
@@ -145,13 +150,15 @@ async fn main() {
                     },
                 }
             }
+            println!("New url: {}", new_path);
             let body = reqwest::get(new_path).await.unwrap().text().await.unwrap();
             Html(body)
         });
         app = app.route(path["path"].as_str().unwrap(), get(m_router));
     }
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
+    println!("{}", local_ip.lock().unwrap().clone().to_owned() + ":3001");
+    let listener = tokio::net::TcpListener::bind(local_ip.lock().unwrap().clone().to_owned() + ":3001").await.unwrap();
     axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
 
 }
