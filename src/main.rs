70ff1fb71dd0a7c193874571425fbc83af64366a
Michael <robo.dude@mail.com>
2024-07-31 18:11:43 +0300

    IP recognisition

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use axum::{
     response::Html, 
     body::Body,
-    extract::ConnectInfo,
+    extract::{ConnectInfo, Request},
     routing::{get, MethodRouter}, 
     Router
 };
@@ -11,16 +11,18 @@ use std::env;
 use std::fs;
 use serde_json::Value;
 use std::cell::Cell;
+use std::rc::Rc;
+use std::sync::Mutex;
 use std::sync::Arc;
 
 
 const ip_size: usize = 40;
 const port_size: usize = 40;
 
-#[derive(Copy, Debug, Clone)]
+#[derive(Debug, Clone)]
 struct IPPortPair {
     ip: [char;ip_size],
-    port: [char;port_size]
+    port: [char;port_size],
 }
 
 impl IPPortPair {
@@ -67,6 +69,7 @@ impl IPPortPair {
             self.ip[index] = ip.chars().nth(index).unwrap();
             self.ip[index+1] = '\0';
         }
+        assert!(self.ip[0] != '\0');
     }
     pub fn is_free(self) -> bool {
         self.ip[0] == '\0'
@@ -89,9 +92,28 @@ async fn main() {
 
     let mut app = Router::new();
     // TODO: Add mutex
-    let mut connections: Cell<[IPPortPair;20]> = Cell::new([IPPortPair::new();20]);
-
-    app = app.fallback(get(error));
+    let mut connections = Arc::new(Mutex::new([();20].map(|_| IPPortPair::new())));
+   
+    let a = connections.clone();
+    let b = connections.clone();
+    app = app.fallback(get(|connection_info: ConnectInfo<SocketAddr>, request: Request<Body> | async move {
+        let mut vec = &mut a.lock().unwrap();
+        let mut ip = format!("{}", connection_info.0);
+        //println!("{:?}", vec);
+        let index = vec.clone().into_iter().position(|x| {x.get_ip() == ip});
+        match index {
+            Some(v) => {
+                let path = vec[v].clone().get_port();
+                let new_path = "http://127.0.0.1:".to_string() + &path;
+                let original_path = request.headers();
+                println!("{:?}", original_path);
+                println!("{}", new_path);
+                return Html("recognized user!");
+            },
+            None => {}
+        }
+        return Html("Forwarder: Error");
+    }));
 
     for path in config.as_array().unwrap() {
         // This part will just basically log the ip and store it. This ip is then used to track
@@ -101,32 +123,32 @@ async fn main() {
         let mut outer_information = Box::new(IPPortPair::new());
         outer_information.set_port(og_port.to_string());
         let new_path = "http://127.0.0.1:".to_string() + &path["port"].as_str().unwrap();
-        let copy_connections = connections.clone();
+        let c = connections.clone();
         let m_router: MethodRouter = get(|connect_info: ConnectInfo<SocketAddr>| async move {
             let ip = connect_info.0;
             let ip_string = format!("{}", ip);
             let mut information = outer_information.clone();
-            let mut vec = copy_connections.get();
-            let existing_ip = vec.into_iter().position(|x| { x.get_ip() == ip_string });
-            match existing_ip {
-                Some(index) => {
-                    vec[index].set_port(information.get_port());
-                    vec[index].set_ip(ip_string);
-                },
-                None => {
-                    let new_index = vec.into_iter().position(|x| { x.is_free() });
-                    match new_index {
-                        Some(value) => {
-                            vec[value].set_port(information.get_port());
-                            vec[value].set_ip(ip_string);
-                        },
-                        None => { panic!("No free slot found"); }
-                    }
-                },
+            {
+                let mut vec = &mut c.lock().unwrap();
+                let existing_ip = vec.clone().into_iter().position(|x| { x.get_ip() == ip_string });
+                match existing_ip {
+                    Some(index) => {
+                        vec[index].set_port(information.get_port());
+                        vec[index].set_ip(ip_string);
+                    },
+                    None => {
+                        let new_index = vec.clone().into_iter().position(|x| { x.is_free() });
+                        match new_index {
+                            Some(value) => {
+                                println!("{} was free", value);
+                                vec[value].set_port(information.get_port());
+                                vec[value].set_ip(ip_string);
+                            },
+                            None => { panic!("No free slot found"); }
+                        }
+                    },
+                }
             }
-            println!("{}", new_path);
-            println!("{:?}", vec);
-            //copy_connections.set(vec);
             let body = reqwest::get(new_path).await.unwrap().text().await.unwrap();
             return Html(body);
         });
